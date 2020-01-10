abeaa3854f756c840c6c44181aa73e256344c2d9
Samat <samat.alpyssov@gmail.com>
2020-01-10 18:42:45 +0600

    Finished week_02 in 02-yellow-belt

--- 02-yellow-belt/week_02/practice_01.cpp
+++ 02-yellow-belt/week_02/practice_01.cpp
@@ -1,1 +1,175 @@
-// Задание по программированию: Декомпозиция программы
+#include <string>
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+enum class QueryType {
+  NewBus,
+  BusesForStop,
+  StopsForBus,
+  AllBuses
+};
+
+struct Query {
+  QueryType type;
+  string bus;
+  string stop;
+  vector<string> stops;
+};
+
+// TODO: WTF?
+istream& operator >> (istream& is, Query& q) {
+  // Реализуйте эту функцию
+    string type, bus, stop;
+    is >> type;
+    if (type == "NEW_BUS") {
+        size_t stop_count;
+        is >> bus >> stop_count;
+        vector<string> stops(stop_count);
+        for (string& stop_name : stops) {
+            is >> stop_name;
+        }
+        q.type = QueryType::NewBus;
+        q.bus = bus;
+        q.stops = stops;
+    } else if (type == "BUSES_FOR_STOP") {
+        is >> stop;
+        q.type = QueryType::BusesForStop;
+        q.stop = stop;
+    } else if (type == "STOPS_FOR_BUS") {
+        is >> bus;
+        q.type = QueryType ::StopsForBus;
+        q.bus = bus;
+    } else if (type == "ALL_BUSES") {
+        q.type = QueryType::AllBuses;
+    }
+
+    return is;
+}
+
+struct BusesForStopResponse {
+  vector<string> buses_for_stop;
+};
+
+ostream& operator << (ostream& os, const BusesForStopResponse& r) {
+  if (r.buses_for_stop.empty()) os << "No stop";
+  else {
+    for (const string& bus_item : r.buses_for_stop) {
+      os << bus_item << " ";
+    }
+  }
+  return os << endl;
+}
+
+struct StopsForBusResponse {
+  string bus;
+  vector<string> stops_for_bus;
+  map<string, vector<string>> stops_to_buses;
+};
+
+ostream& operator << (ostream& os, const StopsForBusResponse& r) {
+  if (r.stops_for_bus.empty()) os << "No bus";
+  else {
+    for (const string& stop_item : r.stops_for_bus) {
+      os << "Stop " << stop_item << ":";
+      if (r.stops_to_buses.at(stop_item).size() == 1) os << "no interchange";
+      else {
+        for (const string& bus_item : r.stops_to_buses.at(stop_item)) {
+          if (bus_item != r.bus) os << " " << bus_item;
+        }
+      }
+    }
+  }
+
+  return os << endl;
+}
+
+struct AllBusesResponse {
+  map<string, vector<string>> buses_to_stops;
+};
+
+ostream& operator << (ostream& os, const AllBusesResponse& r) {
+  if (r.buses_to_stops.empty()) os << "No buses";
+  else {
+    for (const auto& bus_item : r.buses_to_stops) {
+      os << "\nBus " << bus_item.first << ":";
+      for (const string& stop_item : bus_item.second) {
+        os << " " << stop_item;
+      }
+    }
+  }
+  return os << endl;
+}
+
+class BusManager {
+public:
+  void AddBus(const string& bus, const vector<string>& stops) {
+    // buses_to_stops.insert(bus, stops);
+    for (const auto& stop_item : stops) {
+      buses_to_stops[bus].push_back(stop_item);
+      stops_to_buses[stop_item].push_back(bus);
+    }
+  }
+
+  BusesForStopResponse GetBusesForStop(const string& stop) const {
+    BusesForStopResponse response;
+    if (stops_to_buses.count(stop) != 0) {
+      // response.buses_for_stop = stops_to_buses.at(stop);
+      for (const auto& bus : stops_to_buses.at(stop)) {
+        response.buses_for_stop.push_back(bus);
+      }
+    }
+    return response;
+  }
+
+  StopsForBusResponse GetStopsForBus(const string& bus) const {
+    StopsForBusResponse response;
+    if (buses_to_stops.count(bus) != 0) {
+      response.bus = bus;
+      for (const auto& stop : buses_to_stops.at(bus)) {
+        response.stops_for_bus.push_back(stop);
+      }
+      response.stops_to_buses = stops_to_buses;
+    }
+    return response;
+  }
+
+  AllBusesResponse GetAllBuses() const {
+    return AllBusesResponse{buses_to_stops};
+  }
+private:
+  map<string, vector<string>> buses_to_stops;
+  map<string, vector<string>> stops_to_buses;
+};
+
+
+int main() {
+  int query_count;
+  Query q;
+
+  cin >> query_count;
+
+  BusManager manager;
+  for (int i = 0; i < query_count; ++i) {
+    cin >> q;
+    switch (q.type) {
+    case QueryType::NewBus:
+      manager.AddBus(q.bus, q.stops);
+      break;
+    case QueryType::BusesForStop:
+      cout << manager.GetBusesForStop(q.stop) << endl;
+      break;
+    case QueryType::StopsForBus:
+      cout << manager.GetStopsForBus(q.bus) << endl;
+      break;
+    case QueryType::AllBuses:
+      cout << manager.GetAllBuses() << endl;
+      break;
+    }
+  }
+
+  return 0;
+}
--- 02-yellow-belt/week_02/practice_03.cpp
+++ 02-yellow-belt/week_02/practice_03.cpp
@@ -1,1 +1,212 @@
-// Тренировочное задание по программированию: Тесты для класса Person
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+ostream& operator << (ostream& os, const vector<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
+}
+
+template <class T>
+ostream& operator << (ostream& os, const set<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
+}
+
+template <class K, class V>
+ostream& operator << (ostream& os, const map<K, V>& m) {
+  os << "{";
+  bool first = true;
+  for (const auto& kv : m) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << kv.first << ": " << kv.second;
+  }
+  return os << "}";
+}
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+  if (t != u) {
+    ostringstream os;
+    os << "Assertion failed: " << t << " != " << u;
+    if (!hint.empty()) {
+      os << " hint: " << hint;
+    }
+    throw runtime_error(os.str());
+  }
+}
+
+void Assert(bool b, const string& hint) {
+  AssertEqual(b, true, hint);
+}
+
+class TestRunner {
+public:
+  template <class TestFunc>
+  void RunTest(TestFunc func, const string& test_name) {
+    try {
+      func();
+      cerr << test_name << " OK" << endl;
+    } catch (exception& e) {
+      ++fail_count;
+      cerr << test_name << " fail: " << e.what() << endl;
+    } catch (...) {
+      ++fail_count;
+      cerr << "Unknown exception caught" << endl;
+    }
+  }
+
+  ~TestRunner() {
+    if (fail_count > 0) {
+      cerr << fail_count << " unit tests failed. Terminate" << endl;
+      exit(1);
+    }
+  }
+
+private:
+  int fail_count = 0;
+};
+
+class Person {
+public:
+
+  void ChangeFirstName(int year, const string& first_name) {
+    first[year] = first_name;
+  }
+
+  void ChangeLastName(int year, const string& last_name) {
+    last[year] = last_name;
+  }
+
+  string GetFullName(int year) {
+    string name;
+    string first_name = GetFirstName(year);
+    string last_name = GetLastName(year);
+
+    if (first_name.empty() && last_name.empty()) name = "Incognito";
+    else if (first_name.empty()) name = last_name + " with unknown first name";
+    else if (last_name.empty()) name = first_name + " with unknown last name";
+    else name = first_name + " " + last_name;
+
+    return name;
+  }
+
+  string GetFirstName(int year) {
+    string first_name;
+    if (first.find(year) != first.end()) {
+      first_name = first[year];         // if found by the year
+    } else {                            // if not found by the year
+      for (const auto& item : first) {
+        if (item.first >= year) {       // if exceeded the range [begin, year]
+          return first_name;            // -> return the last found name (might be empty)
+        }
+        first_name = item.second;       // assign the name of the previous years
+      }                                 // we may not exceed the range [begin, year]
+    }                                   // when the last key of the map is smaller than year
+    return first_name;                  // -> in this case return the last found name
+  }
+
+  string GetLastName(int year) {
+    string last_name;
+    if (last.find(year) != last.end()) {
+      last_name = first[year];
+    } else {
+      for (const auto& item : last) {
+        if (item.first >= year) {
+          return last_name;
+        }
+        last_name = item.second;
+      }
+    }
+    return last_name;
+  }
+
+  private:
+  map<int, string> first;
+  map<int, string> last;
+};
+
+void TestPersonClass() {
+  // Person person;
+  //
+  // person.ChangeFirstName(1965, "Polina");
+  // person.ChangeLastName(1967, "Sergeeva");
+  // for (int year : {1900, 1965, 1990}) {
+  //   cout << person.GetFullName(year) << endl;
+  // }
+  //
+  // person.ChangeFirstName(1970, "Appolinaria");
+  // for (int year : {1969, 1970}) {
+  //   cout << person.GetFullName(year) << endl;
+  // }
+  //
+  // person.ChangeLastName(1968, "Volkova");
+  // for (int year : {1969, 1970}) {
+  //   cout << person.GetFullName(year) << endl;
+  // }
+}
+
+void TestPredefinedLastNameFirst() {
+    Person person;
+
+    person.ChangeLastName(1965, "Sergeeva");
+    person.ChangeFirstName(1967, "Polina");
+
+    AssertEqual(person.GetFullName(1964), "Incognito");
+    AssertEqual(person.GetFullName(1966), "Sergeeva with unknown first name");
+    AssertEqual(person.GetFullName(1968), "Polina Sergeeva");
+}
+
+void TestPredefined() {
+    Person person;
+
+    person.ChangeFirstName(1965, "Polina");
+    person.ChangeLastName(1967, "Sergeeva");
+
+    AssertEqual(person.GetFullName(1964), "Incognito");
+    AssertEqual(person.GetFullName(1966), "Polina with unknown last name");
+    AssertEqual(person.GetFullName(1968), "Polina Sergeeva");
+
+    person.ChangeFirstName(1969, "Appolinaria");
+    AssertEqual(person.GetFullName(1968), "Polina Sergeeva");
+    AssertEqual(person.GetFullName(1969), "Appolinaria Sergeeva");
+    AssertEqual(person.GetFullName(1970), "Appolinaria Sergeeva");
+
+    person.ChangeLastName(1968, "Volkova");
+    AssertEqual(person.GetFullName(1967), "Polina Sergeeva");
+    AssertEqual(person.GetFullName(1968), "Polina Volkova");
+    AssertEqual(person.GetFullName(1969), "Appolinaria Volkova");
+
+}
+
+int main() {
+  TestRunner runner;
+
+  return 0;
+}
